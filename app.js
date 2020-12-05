30ad942bf356cc287f635c351dd2ed9789e99b6e
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2020-12-05 20:51:17 +0530

    i/o processing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("output");
 
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked");
-    console.log("input",txtInput.value);
+    outputDiv.innerText = "hii my name is " + txtInput.nodeValue;
 })
