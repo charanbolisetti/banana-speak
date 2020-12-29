dfef604c7dc6a9a10929e884db18390909a45591
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2020-12-29 22:21:41 +0530

    updated api

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationUrl(text) {
     return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+    console.log("error occured",error);
+    alert("something went wrong with the server please try again after sometime")
+}
+
 function clickEventHandler() {  
     var inputText = txtInput.value;
+
+    //calling server for processing
     fetch(getTranslationUrl(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
