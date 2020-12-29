f097ecc9baafab9098349d78ce004ab6b8245a41
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2020-12-29 21:37:08 +0530

    feat:added translation

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("output");
+var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputDiv.innerText = "hii my name is " + txtInput.nodeValue;
-})
+function getTranslationUrl(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function clickEventHandler() {  
+    var inputText = txtInput.value;
+    fetch(getTranslationUrl(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
